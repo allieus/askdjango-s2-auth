7f05d132443efd065c5ed807ce37f0d3cbeed0d3
Chinseok Lee <me@askcompany.kr>
2018-07-08 16:45:26 +0900

    EP10: 가입환영 이메일 보내기

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -130,3 +130,11 @@ LOGIN_URL = reverse_lazy('login')
 # LOGIN_REDIRECT_URL = reverse_lazy('profile')
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_HOST = 'localhost'
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_USE_TLS = False
+# EMAIL_USE_SSL = False
+
